cc140834fce74314c77e93e05806d09ba7f78968
Gilson - Grupo 3DS <74361166+grupo3dsbh@users.noreply.github.com>
2025-12-02 04:35:20 -0300

    Update relatorio.php

--- pages/relatorio.php
+++ pages/relatorio.php
@@ -1256,7 +1256,7 @@ jQuery(document).ready(function($) {
                                 <td>
                                     <small>${venda.produto_atual}</small>
                                     ${venda.produto_alterado ? 
-                                        '<br><span class="badge badge-warning badge-sm">Alterado de ' + ${venda.produto_original} + '</span>' : 
+                                        `<br><span class="badge badge-warning badge-sm">Alterado de ${venda.produto_original} </span>` : 
                                         ''}
                                 </td>
                                 <td><small>${venda.data_venda_formatada}</small></td>
@@ -1489,4 +1489,5 @@ jQuery(document).ready(function($) {
 
 
 <?php endif; ?>
+
 
